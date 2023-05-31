e0b6625cabbcf329675080fd5d0f95b574b8669d
Surendar Yama <125799445+SurendarYama@users.noreply.github.com>
2023-05-31 00:31:03 -0700

    first commit
    added useReducer

---
+++ index.js
@@ -0,0 +1,38 @@
+export const joy = () => {
+    const useReducer = (initialState, reducer) => {
+        let globalState = initialState;
+        let state = () => globalState;
+        const isArray = Array.isArray(initialState);
+        const initialStateTypeCheck = (
+            typeof initialState === 'undefined' || 
+            typeof initialState !== 'object' ||
+             isArray
+            );
+        const reducerTypeCheck  = (typeof reducer === 'undefined' || typeof reducer !== 'function');
+        if( initialStateTypeCheck || reducerTypeCheck ) {
+            if(initialStateTypeCheck){
+                throw new Error("InitialState is undefined or typeof initialState must be Object 🧨")
+            } 
+            throw new Error("reducer is undefined 🧨 or typeof reducer must be Function")
+        };
+        let setState = (state) => {
+            return globalState = state;
+        };
+        const dispatch = (action) => {
+            if(typeof action !== 'object' || isArray){
+                throw new Error("typeof action must be Object 🧨");
+            }
+            if(!action.hasOwnProperty('type')){
+                throw new Error("action object must have type property")
+            }
+          setState(reducer(globalState, action))
+        };
+        return [
+            state,
+            dispatch
+        ]
+    };
+    return {
+        useReducer
+    }
+}
