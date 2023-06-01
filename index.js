64d4de31cce984ed0c7297d35abb4ce5d3d04c72
SurendarYama <surendaryama@outlook.com>
2023-06-01 05:43:34 -0700

    typo fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export const joy = () => {
     const useReducer = (initialState, reducer) => {
         let globalState = initialState;
         let state = () => globalState;
-        const isArray = (params) => Array.isArray(params);
+        const isArray = (param) => Array.isArray(param);
         const initialStateTypeCheck = (
             typeof initialState === 'undefined' || 
             typeof initialState !== 'object' ||
@@ -15,15 +15,15 @@ export const joy = () => {
             } 
             throw new Error("reducer is undefined 🧨 or typeof reducer must be Function")
         };
-        let setState = (state) => {
-            return globalState = state;
+        let setState = (mutatedState) => {
+            return globalState = mutatedState;
         };
         const dispatch = (action) => {
             if(typeof action !== 'object' || isArray(action)){
                 throw new Error("typeof action must be Object 🧨");
             }
             if(!action.hasOwnProperty('type')){
-                throw new Error("action object must have type property")
+                throw new Error("action object must have type property 🧨")
             }
           setState(reducer(globalState, action))
         };
